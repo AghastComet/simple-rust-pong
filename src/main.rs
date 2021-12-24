78a772b0a99f7cfd9872a636cddcfa67b15868a5
Sawyer Gibson <sgibson1@mtech.edu>
2021-12-24 10:19:40 -0700

    Gamespeed should not be linked to fps, also easier to config game

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,16 @@ const PADDLE_WIDTH: f32 = 0.5;
 
 const BALL_DIAMETER: f32 = 0.02;
 
+const INITAL_BALL_SPEED: [f32; 2] = [0.006, -0.03];
+const PADDLE_VELOCITY: f32 = 0.01;
+const PLAYER_INITIAL_POSITION: [f32; 2] = [0.0, -0.9];
+const BOT_INITIAL_POSITION: [f32; 2] = [0.0, 0.9];
+const BALL_INITIAL_POSITION: [f32; 2] = [0.0, 0.0];
+
+const BALL_SPEEDUP: f32 = 1.01;
+
+const BALL_REFLECTION_MULTIPLYER: f32 = 0.05;
+
 #[derive(Copy, Clone)]
 struct Vertex{
     position: [f32; 2]
@@ -68,57 +78,21 @@ fn main() {
 
     let program = glium::Program::from_source(&display, vertex_shader_source, fragment_shader_source, None).unwrap();
 
-    let mut player: [f32; 2] = [0.0, -0.9];
-    let mut bot: [f32; 2] = [0.0, 0.9];
-    let mut ball: [f32; 2] = [0.0, 0.0];
+    let mut player: [f32; 2] = PLAYER_INITIAL_POSITION;
+    let mut bot: [f32; 2] = BOT_INITIAL_POSITION;
+    let mut ball: [f32; 2] = BALL_INITIAL_POSITION;
+    let mut ball_velocity: [f32; 2] = INITAL_BALL_SPEED;
 
-    let mut ball_velocity: [f32; 2] = [0.0001, -0.0005];
 
-    let paddle_velocity = 0.0002;
+
 
     let mut left_pressed = false;
     let mut right_pressed = false;
 
+    // let mut previous_frame_time = std::time::Instant::now();
+    let mut next_frame_time = std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667);
+
     event_loop.run(move |ev, _, control_flow|{
-
-        ball[0] += ball_velocity[0];
-        ball[1] += ball_velocity[1];
-
-        if ball[0] - 0.05 > bot[0]{
-            bot[0] = ball[0].min(bot[0] + paddle_velocity);
-        } else if ball[0] + 0.05 < bot[0]{
-            bot[0] = ball[0].max(bot[0] - paddle_velocity);
-        }
-
-        if left_pressed {
-            player[0] -= paddle_velocity;
-        } else if right_pressed {
-            player[0] += paddle_velocity;
-        }
-
-        if ball_velocity[1] < 0. && (ball[1] - player[1]).abs() <= (BALL_DIAMETER+PADDLE_HEIGHT)/2. && (ball[0] - player[0]).abs() <= (BALL_DIAMETER+PADDLE_WIDTH)/2.{
-            // Ball collided with player
-            ball_velocity[0] = (ball[0]-player[0])/PADDLE_WIDTH * 0.00075;
-            ball_velocity[1] *= -1.01;
-        } else if ball_velocity[1] > 0. && (ball[1] - bot[1]).abs() <= (BALL_DIAMETER+PADDLE_HEIGHT)/2. && (ball[0] - bot[0]).abs() <= (BALL_DIAMETER+PADDLE_WIDTH)/2.{
-            ball_velocity[0] = (ball[0]-bot[0])/PADDLE_WIDTH * 0.00075;
-            ball_velocity[1] *= -1.01;
-            // Ball collided with bot
-        } else if ball[0] > 0.99 || ball[0] < -0.99 {
-            // Ball collided with wall
-            ball_velocity[0] = -ball_velocity[0];
-        } else if ball[1] > 1. || ball[1] < -1. {
-            // Ball hit score area
-            // TODO: count scores
-            player = [0.0, -0.9];
-            bot = [0.0, 0.9];
-            ball = [0.0, 0.0];
-            ball_velocity = [0.0001, -0.0005];
-        }
-
-        let next_frame_time = std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667);
-        *control_flow = glium::glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
-
         match ev{
             glium::glutin::event::Event::WindowEvent{event, .. } => match event {
                 glium::glutin::event::WindowEvent::CloseRequested => {
@@ -140,14 +114,58 @@ fn main() {
             _ => (),
         }
 
-        let mut target = display.draw();
+        let current_frame_time = std::time::Instant::now();
+        if current_frame_time >= next_frame_time{
+            let time_delta = (current_frame_time - next_frame_time).as_nanos() as f32 /1000000000. + 1.;
 
-        target.clear_color(0., 0., 0., 1.);
+            next_frame_time = std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667);
+            *control_flow = glium::glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
 
-        target.draw(&paddle_buffer, &indices, &program, &uniform!{offset_position: player}, &Default::default()).unwrap();
-        target.draw(&paddle_buffer, &indices, &program, &uniform!{offset_position: bot}, &Default::default()).unwrap();
-        target.draw(&ball_buffer, &indices, &program, &uniform!{offset_position: ball}, &Default::default()).unwrap();
+            ball[0] += ball_velocity[0] * time_delta;
+            ball[1] += ball_velocity[1] * time_delta;
 
-        target.finish().unwrap();
+            if ball[0] - 0.05 > bot[0]{
+                bot[0] = ball[0].min(bot[0] + PADDLE_VELOCITY * time_delta);
+            } else if ball[0] + 0.05 < bot[0]{
+                bot[0] = ball[0].max(bot[0] - PADDLE_VELOCITY * time_delta);
+            }
+
+            if left_pressed {
+                player[0] -= PADDLE_VELOCITY * time_delta;
+            } else if right_pressed {
+                player[0] += PADDLE_VELOCITY * time_delta;
+            }
+
+            if ball_velocity[1] < 0. && (ball[1] - player[1]).abs() <= (BALL_DIAMETER+PADDLE_HEIGHT)/2. && (ball[0] - player[0]).abs() <= (BALL_DIAMETER+PADDLE_WIDTH)/2.{
+                // Ball collided with player
+                ball_velocity[0] = (ball[0]-player[0])/PADDLE_WIDTH * BALL_REFLECTION_MULTIPLYER;
+                ball_velocity[1] *= -BALL_SPEEDUP;
+            } else if ball_velocity[1] > 0. && (ball[1] - bot[1]).abs() <= (BALL_DIAMETER+PADDLE_HEIGHT)/2. && (ball[0] - bot[0]).abs() <= (BALL_DIAMETER+PADDLE_WIDTH)/2.{
+                ball_velocity[0] = (ball[0]-bot[0])/PADDLE_WIDTH * BALL_REFLECTION_MULTIPLYER;
+                ball_velocity[1] *= -BALL_SPEEDUP;
+                // Ball collided with bot
+            } else if ball[0] > 0.99 || ball[0] < -0.99 {
+                // Ball collided with wall
+                ball_velocity[0] = -ball_velocity[0];
+            } else if ball[1].abs() > 1.{
+                // Ball hit score area
+                // TODO: count scores
+                player = PLAYER_INITIAL_POSITION;
+                bot = BOT_INITIAL_POSITION;
+                ball = BALL_INITIAL_POSITION;
+                ball_velocity = INITAL_BALL_SPEED;
+            }
+
+            let mut target = display.draw();
+
+            target.clear_color(0., 0., 0., 1.);
+
+            target.draw(&paddle_buffer, &indices, &program, &uniform!{offset_position: player}, &Default::default()).unwrap();
+            target.draw(&paddle_buffer, &indices, &program, &uniform!{offset_position: bot}, &Default::default()).unwrap();
+            target.draw(&ball_buffer, &indices, &program, &uniform!{offset_position: ball}, &Default::default()).unwrap();
+
+            target.finish().unwrap();
+        }
+
     });
 }
